18ba8189e39cefe11031f5130ba47a1fc099f891
CongTV <congtv@vnxaco.com>
2017-11-07 15:32:06 +0700

    create project

---
+++ QuanLyDoanVien/QuanLyDoanVien.UI/Areas/FacultyAdmin/FacultyAdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace QuanLyDoanVien.UI.Areas.FacultyAdmin
+{
+    public class FacultyAdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "FacultyAdmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "FacultyAdmin_default",
+                "FacultyAdmin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ QuanLyDoanVien/QuanLyDoanVien.UI/Areas/MasterAdmin/MasterAdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace QuanLyDoanVien.UI.Areas.MasterAdmin
+{
+    public class MasterAdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "MasterAdmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "MasterAdmin_default",
+                "MasterAdmin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
